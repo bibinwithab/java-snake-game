4e9b61d5c1bf411a1aeffcc1743c418505d9e096
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2023-10-03 17:34:52 +0530

    Game over added

--- GameFrame.java
+++ GameFrame.java
@@ -1,8 +1,8 @@
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
-    
-    GameFrame(){
+
+    GameFrame() {
         this.add(new GamePanel());
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- GamePanel.java
+++ GamePanel.java
@@ -10,7 +10,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    static final int DELAY = 75;
+    static final int DELAY = 75; // Adjust this value to change game speed.
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyParts = 6;
@@ -29,78 +29,77 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
         startGame();
-
     }
 
     public void startGame() {
         newApple();
         running = true;
         timer = new Timer();
-        timer.schedule(new TimerTask() {
+        timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'run'");
+                if (running) {
+                    move();
+                    checkApple();
+                    checkCollisions();
+                }
+                repaint();
             }
-        }, DELAY);
-
+        }, 0, DELAY);
     }
 
     public void paintComponent(Graphics g) {
-        super.paintComponents(g);
+        super.paintComponent(g);
         draw(g);
     }
 
     public void draw(Graphics g) {
+        if (running) {
 
-        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.setColor(Color.green);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
-            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-        }
-        g.setColor(Color.green);
-        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-
-        for (int i = 0; i < bodyParts; i++) {
-
-            if (i == 0) {
-                g.setColor(Color.red);
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-            } else {
-                g.setColor(new Color(255, 106, 95));
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    g.setColor(Color.red);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(new Color(255, 106, 95));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
             }
+            g.setColor(Color.white);
+            g.setFont(new Font("Arial", Font.BOLD, 40));
+            FontMetrics metrics2 = getFontMetrics(g.getFont());
+            g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: " + applesEaten)) / 2,
+                    g.getFont().getSize());
+        } else {
+            gameOver(g);
         }
+
     }
 
     public void newApple() {
         appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
         appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
-
     }
 
     public void move() {
-
         for (int i = bodyParts; i > 0; i--) {
-
             x[i] = x[i - 1];
             y[i] = y[i - 1];
-
         }
 
         switch (direction) {
             case 'U':
                 y[0] = y[0] - UNIT_SIZE;
                 break;
-
             case 'D':
                 y[0] = y[0] + UNIT_SIZE;
                 break;
-
             case 'L':
                 x[0] = x[0] - UNIT_SIZE;
                 break;
-
             case 'R':
                 x[0] = x[0] + UNIT_SIZE;
                 break;
@@ -108,29 +107,77 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkApple() {
-
+        if (x[0] == appleX && y[0] == appleY) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
 
     public void checkCollisions() {
+        // Check if the snake hits the borders of the screen
+        if (x[0] < 0 || x[0] >= SCREEN_WIDTH || y[0] < 0 || y[0] >= SCREEN_HEIGHT) {
+            running = false;
+        }
 
+        // Check if the snake collides with itself
+        for (int i = 1; i < bodyParts; i++) {
+            if (x[i] == x[0] && y[i] == y[0]) {
+                running = false;
+            }
+        }
+
+        if (!running) {
+            timer.cancel();
+        }
     }
 
     public void gameOver(Graphics g) {
+        // Game Over text
+        g.setColor(Color.red);
+        g.setFont(new Font("Arial", Font.BOLD, 75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
 
+        // Score text
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.BOLD, 40));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics2.stringWidth("Score: " + applesEaten)) / 2,
+                g.getFont().getSize());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R')
+                        direction = 'L';
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L')
+                        direction = 'R';
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D')
+                        direction = 'U';
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U')
+                        direction = 'D';
+                    break;
+            }
         }
     }
-
 }
